c6256ca2d7f987638bd061fb27c94736b1dba447
Matthew DeMichele <Matt@Matthews-Air.lan>
2022-02-24 13:42:10 -1000

    added microservice stuff

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ app.get('/bike-racks', (req, res) => {
         
         // Add to bikeRacks array 
         bikeRacks.push(currentBike);
-          
         }
       })
       .on('end', function() {
@@ -55,6 +54,7 @@ app.get('/bike-racks', (req, res) => {
         process.stdout.write("Done Processing");
       })
 });
+
 
 // Receives a specific address and delivers back the lat/long 
 app.post('/forward-geocode', async (req, res) => {
@@ -73,6 +73,7 @@ app.post('/forward-geocode', async (req, res) => {
   
 });
 
+
 // Receives a lat and long and delivers a url of a map 
 app.post('/microservice', async (req, res) => {
   let lat = req.body.lat;
@@ -83,6 +84,122 @@ app.post('/microservice', async (req, res) => {
   res.status(200).json({ url: url });
 });
 
+
+// Calls carlos' microservice 
+app.get('/get-graph', async (req, res) => {
+  // Declare a variable to skip the column label line
+  let skipFirstLine = false;
+  
+  // All the neighborhoods in Seattle 
+  let bikeNeighborhoods = {
+    broadview: 0,
+    northgate: 0,
+    lakeCity: 0,
+    greenwood: 0,
+    wedgwood: 0,
+    ballard: 0,
+    phinneyRidge: 0,
+    roosevelt: 0,
+    universityDistrict: 0,
+    ravenna: 0,
+    viewRidge: 0,
+    fremont: 0,
+    wallingford: 0,
+    magnolia: 0,
+    interbay: 0,
+    queenAnne: 0,
+    lowerQueenAnne: 0,
+    eastlake: 0,
+    montlake: 0,
+    madisonPark: 0,
+    southLakeUnion: 0,
+    capitolHill: 0,
+    madrona: 0,
+    belltown: 0,
+    downtown: 0,
+    firstHill: 0,
+    centralDistrict: 0,
+    leschi: 0,
+    pioneerSquare: 0,
+    Chinatown: 0,
+    sodo: 0,
+    beaconHill: 0,
+    mountBaker: 0,
+    westSeattle: 0,
+    delridge: 0,
+    columbiaCity: 0,
+    sewardPark: 0,
+    southPark: 0,
+    GeorgeTown: 0,
+    rainierValley: 0
+  }
+  let neighborhoodKeys = Object.keys(bikeNeighborhoods);
+  
+  // Read through each line of the csv and insert the serialized data into bikeRacks 
+  fs.createReadStream('city-of-seattle-bicycle-racks.csv')
+      .pipe(parse({delimiter: ','}))
+      .on('data', function(row) {
+        // This ensures that the first line (the column labels) is skipped
+        if (skipFirstLine == false) {
+          skipFirstLine = true;
+        }   
+        // Process every remaining line 
+        else {
+          // Choose a random neighborhood and increment its bike count 
+          let currentIndex = parseInt(Math.random() * neighborhoodKeys.length);
+          bikeNeighborhoods[neighborhoodKeys[currentIndex]] += 1;  
+        }
+      })
+      .on('end', async function() {
+        
+        // Populate Keys 
+        const keys = Object.keys(bikeNeighborhoods);
+
+        // Populate Values 
+        const vals = []
+        for (let i = 0; i < keys.length; i++) {
+          vals.push(bikeNeighborhoods[keys[i]]);
+        }
+
+        // Create template 
+        const template = {
+          config: {
+            type: 'bar',
+            data: {
+              labels: keys,
+              datasets: [
+                {
+                  data: vals
+                },
+              ],
+            },
+            options: {
+              legend: { display: false },
+              title: { display: true, text: 'Bike Racks By Neighborhood' },
+            }
+          },
+          width: 320,
+          height: 240
+        }
+        
+        const path = 'http://localhost:5000/generateGraph';
+        
+        const response = await axios({
+          method: 'post',
+          url: path, 
+          data: template,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        });
+        
+        const val = await response.data;
+        
+        res.status(200).json({url: val.graph_url });
+      });
+})
+
 app.listen(8000, () => {
-  console.log("Server listening. Get after it!");
+  console.log(`Server listening. Get after it! Port ${8000}`);
 });
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import Map from "../components/Map";
 import Search from "../components/Search";
 import Header from '../components/Header';
+import axios from 'axios';
 
 function HomePage() {
   const [searchCoordinates, setSearchCoordinates] = useState({lon: -122.34, lat: 47.6062}); // Default long/lat of Seattle 
   const [address, setAddress] = useState("");
+  const [graph, setGraph] = useState("");
+  
+  useEffect(async () => {
+    const response = await axios.get("/get-graph");
+    setGraph(response.data.url);
+  });
   
   return (
     <div className="home-page">
@@ -21,12 +29,12 @@ function HomePage() {
       <Search searchCoordinates={searchCoordinates} setSearchCoordinates={setSearchCoordinates} address={address} setAddress={setAddress}/>
       
       {/* Bike Rack Map */}
-      <button className="refresh-button">Refresh Map</button>
+      <button className="refresh-button"><a href="/">Refresh Map</a></button>
       <Map searchCenter={searchCoordinates}/>
       
       {/* Teammate Microservice */}
       <div className="team-microservice-container">
-        <button className="team-microservice">See Graph of bike racks</button>
+        <button className="team-microservice"><a href={graph} target="_blank">See Graph of bike racks</a></button>
       </div>
     </div>
   )
